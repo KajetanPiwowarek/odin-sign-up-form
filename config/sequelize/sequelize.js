be8d284d85153ba48b11045a23ad48ada5d7ce86
Kajetan Piwowarek <kajtek@piwowarek.com>
2023-11-25 17:51:13 +0100

    sequelize update

--- config/sequelize/sequelize.js
+++ config/sequelize/sequelize.js
@@ -2,34 +2,37 @@ const { SecretClient } = require("@azure/keyvault-secrets");
 const { DefaultAzureCredential } = require("@azure/identity");
 require('dotenv').config();
 
-const keyVaultName = "project-inz-keyVault";
-const keyVaultUri = `https://${keyVaultName}.vault.azure.net`;
+async function createSequelizeInstance() {
+  const keyVaultName = "project-inz-keyVault";
+  const keyVaultUri = `https://${keyVaultName}.vault.azure.net`;
 
-async function getSecret(secretName) {
-  // Ustalanie, czy kod jest uruchamiany lokalnie czy na Azure
-  if (process.env.AZURE_FUNCTIONS_ENVIRONMENT === 'Development') {
-    // Użyj process.env lokalnie
-    return process.env[secretName];
-  } else {
-    // Użyj Azure Key Vault na platformie Azure
-    const credential = new DefaultAzureCredential();
-    const secretClient = new SecretClient(keyVaultUri, credential);
-    const secret = await secretClient.getSecret(secretName);
-    return secret.value;
+  async function getSecret(secretName) {
+    // Ustalanie, czy kod jest uruchamiany lokalnie czy na Azure
+    if (process.env.AZURE_FUNCTIONS_ENVIRONMENT === 'Development') {
+      // Użyj process.env lokalnie
+      return process.env[secretName];
+    } else {
+      // Użyj Azure Key Vault na platformie Azure
+      const credential = new DefaultAzureCredential();
+      const secretClient = new SecretClient(keyVaultUri, credential);
+      const secret = await secretClient.getSecret(secretName);
+      return secret.value;
+    }
   }
+
+  const sequelize = new Sequelize(await getSecret('dbName'), await getSecret('dbUser'), await getSecret('dbPassword'), {
+    host: await getSecret('dbHost'),
+    dialect: 'mssql',
+    dialectOptions: {
+      options: {
+        encrypt: true,
+      },
+    },
+  });
+
+  return sequelize;
 }
 
-// Wykorzystanie w kodzie Sequelize
-const Sequelize = require('sequelize');
+module.exports = createSequelizeInstance;
 
-const sequelize = new Sequelize(await getSecret('dbName'), await getSecret('dbUser'), await getSecret('dbPassword'), {
-  host: await getSecret('dbHost'),
-  dialect: 'mssql',
-  dialectOptions: {
-    options: {
-      encrypt: true,
-    },
-  },
-});
 
-module.exports = sequelize;
